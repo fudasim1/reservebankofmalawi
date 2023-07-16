403a55475c7f429098e7d813b1d0c6139111a7de
fudasim1 <139609386+fudasim1@users.noreply.github.com>
2023-07-16 13:51:09 +0200

    Create App.java

---
+++ app/src/main/java/com/reservebankofmalawi/App.java
@@ -0,0 +1,14 @@
+package com.reservebankofmalawi;
+
+import android.app.Application;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
